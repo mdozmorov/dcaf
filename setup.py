054a15faedd7596e24f4a4834b9704a978952569
Cory Giles <mail@corygil.es>
2013-11-03 14:06:57 -0600

    Prepare setup.py for upload to PyPI.

--- setup.py
+++ setup.py
@@ -1,28 +1,48 @@
+import glob
+import subprocess
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
-import glob
 
 scripts = ["script/genome_runner"]
 
+def version():
+    return subprocess.check_output(["git", "describe", "--tags"]).decode("ascii").strip()
+
+extensions = [
+        Extension(
+            "dcaf.bbi",
+            include_dirs=["src"],
+            sources=["dcaf/bbi.pyx", "src/bbi.cpp", "src/mm.cpp"],
+            extra_compile_args=["-std=c++0x"],
+            libraries=["z"],
+            language="c++")
+        ]
+
 setup(
         name="dcaf",
-        version="0.0.1-HEAD",
-        author="Mikhail Dozmorov; Cory Giles",
-        author_email="dozmorovm@omrf.org; mail@corygil.es",
-        description="General Wren Lab scripts and utilities.",
+        version=version(),
+        author="Cory Giles, Mikhail Dozmorov",
+        author_email="mail@corygil.es; dozmorovm@omrf.org",
+        description="Utilities for genome analysis, expression analysis, and text mining used by the Wren Lab at the Oklahoma Medical Research Foundation.",
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+            "Natural Language :: English",
+            "Operating System :: POSIX",
+            "Programming Language :: Python",
+            "Programming Language :: C++",
+            "Programming Language :: Unix Shell",
+            "Programming Language :: SQL",
+            "Topic :: Scientific/Engineering :: Bio-Informatics"
+        ]
+        license="AGPLv3+",
 
         packages=["dcaf"],
         scripts=scripts,
         cmdclass= {"build_ext": build_ext},
-        ext_modules = [
-            Extension(
-                "dcaf.bbi",
-                include_dirs=["src"],
-                sources=["dcaf/bbi.pyx", "src/bbi.cpp", "src/mm.cpp"],
-                extra_compile_args=["-std=c++0x"],
-                libraries=["z"],
-                language="c++")
-            ]
+        ext_modules = extensions
 )
