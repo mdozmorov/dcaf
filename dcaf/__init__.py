12a38300429ea27e43fb2f08f972e9306fba1401
Cory Giles <mail@corygil.es>
2013-11-03 19:27:04 -0600

    Delete genome-runner script (now autogenerated) and move old grtk modules in.

--- dcaf/__init__.py
+++ dcaf/__init__.py
@@ -6,10 +6,11 @@ __author_email__ = "mail@corygil.es"
 
 # FIXME: This obviously won't work outside of a git clone
 try:
-    __version__ = subprocess.check_output("git --git-dir %s describe --tags" % \
-                                          os.path.join(os.path.dirname(__file__), "../.git"),
-                                          stderr=os.devnull,
-                                          shell=True).decode("ascii").strip()
+    with open(os.devnull, "w") as devnull:
+        __version__ = subprocess.check_output("git --git-dir %s describe --tags" % \
+                                              os.path.join(os.path.dirname(__file__), "../.git"),
+                                              stderr=devnull,
+                                              shell=True).decode("ascii").strip()
 except:
     try:
         from dcaf.version import __version__
