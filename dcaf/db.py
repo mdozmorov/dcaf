c5eb3936eb8c3c4288eff40e5789fda2b588f527
Cory Giles <mail@corygil.es>
2013-11-08 17:07:29 -0600

    Start MEDLINE import script translation.

--- dcaf/db.py
+++ dcaf/db.py
@@ -351,7 +351,19 @@ class UCSCConnection(Connection):
     CFG_SECTION = "UCSC"
 
 if __name__ == "__main__":
-    pass
+    import gzip
+    import dcaf.io
+    import lxml.etree
+
+    xslt = lxml.etree.XSLT(lxml.etree.parse(dcaf.io.data("xslt/article.xsl")))
+    root = xslt(dom)
+
+    with gzip.open("/home/gilesc/data/text/medline13n0001.xml.gz") as h:
+        dom = lxml.etree.parse(h)
+
+    for row in io.StringIO(str(root)):
+        print(row)
+
     #db = DCAFConnection.from_configuration()
     #top_coexpressed()
     #db.load_schema()
