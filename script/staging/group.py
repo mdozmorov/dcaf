8179a89ace9c1cebb9befe1538f91c157578b8b3
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2014-01-06 21:30:06 -0600

    Experimenting with groupby function

--- script/staging/group.py
+++ script/staging/group.py
@@ -12,3 +12,18 @@ for k, grp in groupby(rows, lambda r: [r[0],r[1],r[2]]):
         else:
             result.append("|".join(col))
     print "\t".join(result)
+
+# Comment out the melow code, used within python	
+# A special version of groupby made by understanding the functions
+rows = [line.strip().split("\t") for line in open('gwas808.catInfo.txt')]
+# Test groupby
+for diseases, snps in groupby(rows, lambda r: r[1]):
+	listOfSnps = "\t".join(["%s" % snp[0] for snp in snps])
+	print(diseases + "s: " + listOfSnps)
+# Now, use groupby to output separate sets of SNPs
+for diseases, snps in groupby(rows, lambda r: r[1]):
+    listOfSnps = [] # For each disease, initialize empty list
+    for snp in snps: listOfSnps.append(snp[0]) # Append to it corresponding SNPs selected by groupby
+    with open(diseases + ".txt", "w") as out: # Open disease-specific file
+        for snp in listOfSnps: # Write out each SNP
+            out.write(str(snp) + "\n") # and include new line
